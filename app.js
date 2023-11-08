347230affa59743db0936ccdb48c79df8d41df9e
govin-10 <govindatimsina@outlook.com>
2023-11-08 22:23:12 +0545

    Day 2 practise

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+//Day 2
+
+//paili internal server banayau
 const express = require("express");
 const app = express();
 
+//aba view engine pani setup gareko
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Welcome to the first rendered site from Node.");
+  res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
 });
 
 app.listen(3000, () => {
-  console.log("Server Started at port 3000.");
+  console.log("Server started at port 3000.");
 });
+
+//tyasapchhi node save garne bittikai server reload garna hamile nodemon install garyau
